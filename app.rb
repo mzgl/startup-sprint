09dd675dbfe82fb8fdb1855418a5067d14790c3f
mzgl <m.ghillany@gmail.com>
2016-06-21 15:36:33 -0400

    reddit code in app

--- app.rb
+++ app.rb
@@ -19,7 +19,21 @@ class App < Sinatra::Base
 
   get '/reddit' do
     
-    @listings = []
+    require 'httparty'
+
+    url = 'http://reddit.com/.json'
+    response = HTTParty.get(url)
+    parsed = response.parsed_response
+    @listings = Array.new
+
+    for x in 1..24
+      title = parsed['data']['children'][x]['data']['title']
+      puts x
+      puts @listings
+      @listings.push(title)
+    end
+
+@listings
 
     erb :reddit
   end
